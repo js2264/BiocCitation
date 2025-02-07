7197fe44201ec792bb9d3ee769fedeee476e7ba8
js2264 <jacquesserizay@gmail.com>
2025-02-07 11:47:14 +0100

    fix: main function

--- R/updateCitation.R
+++ R/updateCitation.R
@@ -15,39 +15,66 @@
 #' }
 #' 
 #' @importFrom rcrossref cr_cn
+#' @importFrom stringr str_glue
 #' 
 #' @export
 
 updateCitation <- function(doi, output_file = "inst/CITATION") {
-  
+    
+    # Check that output folder exists
+    if (!dir.exists(dirname(output_file))) {
+        stop(dirname(output_file), " directory does not exist.")
+    }
+    # Ask if it's ok to replace current CITATION
+    if (file.exists(output_file)) {
+        response <- readline(prompt = paste(output_file, "already exists. Do you want to replace it? (y/n): "))
+        if (tolower(response) != "y") {
+            stop("Operation cancelled by the user.")
+        }
+    }
+
+    # Fetch citation information
     citation_info <- rcrossref::cr_cn(dois = doi, format = "citeproc-json")
+
+    # Check success
+    if (is.null(citation_info)) {
+        stop("DOI not found.")
+    }
+
+    # Format citation information
     title <- citation_info$title
-    authors <- apply(citation_info$author, 1, function(aut) paste(aut$given, aut$family))
-    authors <- paste(authors, collapse = ",\n        ")
+    authors <- apply(citation_info$author, 1, function(aut) {
+        paste(
+            "as.person(\"", 
+            paste(aut$given, aut$family), 
+            "\")", 
+            sep = ""
+        )
+    }) |> paste(collapse = ",\n        ")
     journal <- citation_info$`container-title`
     year <- citation_info$issued$`date-parts`[[1]][1]
     volume <- citation_info$volume
     issue <- citation_info$issue
     pages <- citation_info$page
     
-    citation_content <- paste(
+    citation_content <- stringr::str_glue(
         "bibentry(\n",
         "  bibtype = \"Article\",\n",
         "  author = c(\n",
-        "    ", authors, "\n",
+        "    {authors}\n",
         "  ),\n",
-        "  title = \"", title, "\",\n",
-        "  journal = \"", journal, "\",\n",
-        "  year = \"", year, "\",\n",
-        "  volume = \"", volume, "\",\n",
-        "  issue = \"", issue, "\",\n",
-        "  pages = \"", pages, "\",\n",
-        "  doi =  \"", doi, "\"\n",
-        ")\n",
-        sep = ""
+        "  title = \"{title}\",\n",
+        "  journal = \"{journal}\",\n",
+        "  year = \"{year}\",\n",
+        "  volume = \"{volume}\",\n",
+        "  issue = \"{issue}\",\n",
+        "  pages = \"{pages}\",\n",
+        "  doi =  \"{doi}\"\n",
+        ")\n"
     )
-  
+    
+    # Write to file
     writeLines(citation_content, con = output_file)
-  
+    
     message("CITATION file created successfully.")
 }
